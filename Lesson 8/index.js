1e126dd04b1dcb44c4185be8d0917f9cffb9b16e
Shjinn <thanh03951@example.com>
2025-04-10 22:06:43 +0700

    Lesson 8

---
+++ Lesson 8/index.js
@@ -0,0 +1,91 @@
+
+// Lesson 8 : 
+
+
+// let names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// for (let i = names.length - 1; i >= 3; i--) {
+//     names[i + 1] = names[i];
+// }
+// names[3] = "MindX";
+// console.log(names)
+
+// // Bài 1
+// const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// console.log(names[0]);
+// console.log(names[4]);
+
+// // Bài 2
+// const names = ["John", "Jane", "Jim", "Jake"];
+// names[1] = "Mary";
+// console.log(names);
+
+// // Bài 3 
+// const names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+// // Bài 4
+// const names = ["Alice", "Bob", "Charlie", "David"];
+// names.push("MindX");
+// console.log(names);
+
+// // Bài 5
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 !== 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+// // Bài 6
+// const names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+// for (let i = 0; i < names.length; i++) {
+//     if (names[i] == "David") {
+//         console.log(names[i]);
+//     }
+// }
+
+// // Bài 7
+// const numbers = [1, 2, 3, 4, 2, 1, 3, 2, 4, 2, 2, 4, 5, 1, 3, 7];
+// const targetCounting = 4;
+// let count = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] === targetCounting) {
+//         count++;
+//     }
+// } console.log(count);
+
+
+// // Bài 8
+// const numbers = [5, 2, 9, 3, 7, 11, 8];
+// let max = numbers[0];
+// for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//         max = numbers[i];
+//     }
+// }
+// console.log(max);
+
+// // Bài 9 
+// const array = [1, 2, 3, 4, 5];
+// const newArray = [];
+// for (let i = array.length - 1; i >= 0; i--) {
+//     newArray.push(array[i]);
+// }
+// console.log(newArray); 
+
+
+// // Bài 10 
+// const duplicatesArray = [1, 2, 2, 3, 4, 4, 5];
+// const newArray = [];
+// for (let i = 0; i < duplicatesArray.length; i++) {
+//     if (!newArray.includes(duplicatesArray[i])) {
+//         newArray.push(duplicatesArray[i]);
+//     }
+// }
+// console.log(newArray); 
+
+
+
